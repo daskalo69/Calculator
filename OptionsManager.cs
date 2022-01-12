202f54f8f224b94efbd96e438fea4d297a79d6e1
daskalo69 <github1699@gmail.com>
2022-01-12 08:33:19 +0200

    calc update

---
+++ OptionsManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace GitExercise
+{
+    public static class OptionsManager
+    {
+        public static string[] OptionsList = {
+            "a - Add",
+            "s - Subtract",
+            "m - Multiply",
+            "d - Divide"
+        };
+
+        public static void Add(double a, double b)
+        {
+            Console.WriteLine($"{a} + {b} = {a + b}");
+        }
+
+        public static void Subtract(double a, double b)
+        {
+            Console.WriteLine($"{a} - {b} = {a - b}");
+        }
+
+        public static void Multiply(double a, double b)
+        {
+            Console.WriteLine($"{a} * {b} = {a * b}");
+        }
+
+        public static void Divide(double a, double b)
+        {
+            Console.WriteLine($"{a} / {b} = {a / b}");
+        }
+
+
+    }
+}
